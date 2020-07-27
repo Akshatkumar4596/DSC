2e4e466fdce63ad84beefe1f9b0546489f982315
Akshatoxy <55821074+Akshatoxy@users.noreply.github.com>
2020-07-27 13:05:52 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,9 +3,18 @@ window.onscroll = function(e) {
     console.log(window.scrollY);
     if(window.scrollY>0){
         document.getElementById("navbar").style.backgroundColor = "rgb(53, 54, 58,0.99)";    
+
         document.getElementById("navbar").style.transition = "background-color 1s ease"; 
+
     }
-    else{
+    else{  
         document.getElementById("navbar").style.backgroundColor = "rgb(0,0,0,0.2)";
     }
 }
+
+var navlink = document.querySelector(".nav-links");
+var menu = document.querySelector(".menu-icon");
+
+menu.addEventListener("click",function(){
+    navlink.classList.toggle('open');
+})
