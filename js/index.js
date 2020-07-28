434bd9ee652e34e7f4f50f02a1120308f4b5431e
Akshatoxy <55821074+Akshatoxy@users.noreply.github.com>
2020-07-28 09:29:11 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ console.log("HEY");
 window.onscroll = function(e) {
     console.log(window.scrollY);
     if(window.scrollY>0){
-        document.getElementById("navbar").style.backgroundColor = "rgb(53, 54, 58,0.99)";    
+        document.getElementById("navbar").style.backgroundColor = "rgb(53, 54, 58)";    
 
         document.getElementById("navbar").style.transition = "background-color 1s ease"; 
 
